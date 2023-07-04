f210a818188338a022408fa06199efa74d8c6c55
filippofinke <filippofinke@protonmail.ch>
2023-07-04 11:15:25 +0200

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sta_parser',
+    version='1.0.0',
+    author='Filippo Finke',
+    author_email='filippofinke@protonmail.ch',
+    description='A parser for the OGC SensorThings API standard',
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
